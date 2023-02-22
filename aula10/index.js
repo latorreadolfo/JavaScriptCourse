41e7bef92ad30c332e6557c5439f8a47d1d9d0cc
latorreadolfo <adolfo3eng@gmail.com>
2023-02-22 20:12:03 -0300

    primitive types of data class

---
+++ aula10/index.js
@@ -0,0 +1,19 @@
+// Primitive types of data: string, number, undefined, null, boolean
+
+const name = 'Adolfo';      //string
+const name1 = "Adolfo";     //string
+const name2 = `Adolfo`;     //string
+const num = 10;            //number
+const num1 = 10.52;         //number
+let userName;               //undefined declaration, doesn't go anywhere
+const userLastName = null;  //null value, doesn't go anywhere either
+const aproved = true;       //boolean value, it can be true or false, nothing else.
+
+console.log(typeof name, name);
+console.log(typeof name1, name1);
+console.log(typeof name2, name2);
+console.log(typeof num, num);
+console.log(typeof num1, num1);
+console.log(typeof userName, userName);
+console.log(typeof userLastName, userLastName);
+console.log(typeof aproved, aproved);
