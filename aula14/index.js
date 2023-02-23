ae6698b53b4f3e30e9b6b769cb227ba59821e9d1
latorreadolfo <adolfo3eng@gmail.com>
2023-02-23 12:28:13 -0300

    reviewing all string methods

---
+++ aula14/index.js
@@ -0,0 +1,16 @@
+let myString = "Je";
+
+// concat options:
+
+console.log(myString.concat(' me souviens.'));
+console.log(myString + ' me souviens.');
+console.log(`${myString} me souviens.`);
+
+let myText = "Learning to code every day.";
+
+console.log(myText.length);
+console.log(myText.replace('day', 'night'));
+console.log(myText.slice(-10));
+console.log(myText.toUpperCase());
+console.log(myText.toLowerCase());
+console.log(myText.split(' '));
