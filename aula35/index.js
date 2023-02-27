fdf11247be92a6ba2b3cf9005dbdba248383a244
latorreadolfo <adolfo3eng@gmail.com>
2023-02-27 20:02:38 -0300

    classic for class

---
+++ aula35/index.js
@@ -0,0 +1,17 @@
+const fruits = ['Apple', 'Pineapple', 'Strawberry', 'Watermelon', 'Mango'];
+for (let i=0; i < fruits.length; i++) {
+    console.log(`Today we have: ${fruits[i]}`);
+}
+
+/*              // -> classic for loop selecting pair numbers
+for (let i=0; i <= 20; i++) {
+    const pair = i % 2 === 0 ? 'Pair' : 'Not Pair';
+    console.log(i, pair);
+}
+*/
+
+/*              // -> classic for loop example
+for (let i = 1; i <= 50; i++) {
+    console.log(`for loop is printing this for you ${i} times.`)
+}
+*/
