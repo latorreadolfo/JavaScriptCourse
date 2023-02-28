e500e75eeb79fae819a7870647b580cdcd80e30e
latorreadolfo <adolfo3eng@gmail.com>
2023-02-28 11:35:02 -0300

    adicional comments in error treatment file

--- aula43/lesson43errortreatment.js
+++ aula43/lesson43errortreatment.js
@@ -8,7 +8,7 @@ function sum(x, y) {
 try {
 console.log(sum(1, 2));
 console.log(sum('1', 2));
-} catch(e) {
-    // console.log(e);          // never show the error for the user, you must show this only for the backend
+} catch(e) {                    // -> this runs only if the section "try" has an error 
+    // console.log(e);          // -> never show the error for the user, you must show this only for the backend
     console.log(`Error: Incredible friendly message for the User <3`);
 }
