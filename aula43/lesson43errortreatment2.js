bcec6cdb8e1a60583148be939a658a4f51c39844
latorreadolfo <adolfo3eng@gmail.com>
2023-02-28 20:53:40 -0300

    improvements to try catch lesson file

--- aula43/lesson43errortreatment2.js
+++ aula43/lesson43errortreatment2.js
@@ -1,3 +1,31 @@
+function hourFeedback(date) {
+    if (date && !(data instanceof Date)) {
+        throw new TypeError('Waiting for date stance.');
+    }
+
+    if (!date) {
+        date = new Date();
+    }
+
+    return date.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    });
+}
+
+try {
+    const date = new Date('01-01-1970 12:45:45');
+    const hour = hourFeedback();
+    console.log(hour);
+} catch(e) {
+    // treat error
+} finally {
+    console.log('Have a good one!');
+}
+
+/*
 try {
     console.log('Opens a file');
     console.log('Manipulates a file');
@@ -13,5 +41,5 @@ try {
 } catch (e) {
     console.log('Dealing with the error...');
 } finally {
-    console.log('FINALLY: I always execute, no matters what');
-}
+    console.log('FINALLY: I am always executed, no matters what');
+}*/
