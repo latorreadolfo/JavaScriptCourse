85a2db67599df61ef3af99d6952a726e5d228e4a
latorreadolfo <adolfo3eng@gmail.com>
2023-02-28 11:32:29 -0300

    error treatment lesson

---
+++ aula43/lesson43errortreatment.js
@@ -0,0 +1,14 @@
+function sum(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        throw new ReferenceError(`x and y needs to be a number, pal.`)
+    }
+    return x + y;
+}
+
+try {
+console.log(sum(1, 2));
+console.log(sum('1', 2));
+} catch(e) {
+    // console.log(e);          // never show the error for the user, you must show this only for the backend
+    console.log(`Error: Incredible friendly message for the User <3`);
+}
