6354c6ef78bbb0d467aeab348d94582069529786
latorreadolfo <adolfo3eng@gmail.com>
2023-03-06 11:01:05 -0300

    array-splice methods

---
+++ aula59/advancedarray1.js
@@ -0,0 +1,13 @@
+// SPLICE -> "arrayvar".splice(index, delete, elem1, elem2, elem3);
+
+const names = ['Michael', 'Jim', 'Pam', 'Dwight', 'Kevin'];
+
+
+// -> unshift
+// names.splice(0, 0, 'Gabe');
+// -> push
+// names.splice(names.length, 0, 'Oscar');
+// -> pop
+// names.splice(-1, 1);
+
+console.log(names);
