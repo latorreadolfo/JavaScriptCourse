c028912a730490533c5645b7730d90ed78f933dd
latorreadolfo <adolfo3eng@gmail.com>
2023-03-08 11:52:45 -0300

    validating documents

---
+++ aula73/validatingdocument.js
@@ -0,0 +1,52 @@
+//    705.484.450-52 070.987.720-03
+
+function ValidateCPF(sentCPF) {
+    Object.defineProperty(this, 'cleanCPF', {
+        enumerable: true,
+        get: function() {
+            return sentCPF.replace(/\D+/g, '');
+        }
+    });
+}
+
+ValidateCPF.prototype.validate = function () {
+    if (typeof this.cleanCPF === 'undefined') return false;
+    if(this.cleanCPF.length !== 11) return false;
+    if(this.isSequencial()) return false;
+
+    const cpfvalues = this.cleanCPF.slice(0, -2);
+    const digit1 = this.createDigit(cpfvalues);
+    const digit2 = this.createDigit(cpfvalues + digit1);
+
+    const newCPF = cpfvalues + digit1 + digit2;
+
+    return newCPF === this.cleanCPF;
+};
+
+ValidateCPF.prototype.createDigit = function (cpfvalues) {
+    const cpfArray = Array.from(cpfvalues);
+
+    let regressive = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressive * Number(val));
+        regressive--;
+        return ac;
+    }, 0);
+
+    const digit = 11 - (total % 11);
+    return digit > 9 ? '0' : String(digit);
+};
+
+ValidateCPF.prototype.isSequencial = function () {
+    const sequence = this.cleanCPF[0].repeat(this.cleanCPF.length);
+    return sequence === this.cleanCPF;
+};
+
+const cpf = new ValidateCPF('705.484.450-52');
+
+
+if (cpf.validate()) {
+    console.log('Your CPF is valid');
+}else{
+    console.log('Your CPF is not valid');
+}
