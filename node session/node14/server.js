f55552521507b59e677b64a959cb21dcb0b9b25b
latorreadolfo <adolfo3eng@gmail.com>
2023-03-15 11:23:38 -0300

    Node.js + Connection on MongoDB

---
+++ node session/node14/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        app.emit('success');
+    })
+    .catch(e => console.log(e));
+
+
+
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal } = require('./src/middlewares/middleware');
+
+app.use( express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+// OUR MIDDLEWARES
+app.use(middlewareGlobal);
+app.use(routes);
+
+app.on('success', () => {
+    app.listen(3000, () => {
+        console.log('Server Running on Port: 3000');
+        console.log('Access on http://localhost:3000');
+    });
+
+});
+
